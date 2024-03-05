63ff10f1d95a384462f1f44ecc619023ca636583
13xforever <ilya.veselov@gmail.com>
2024-03-05 16:24:55 +0500

    update publish script

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,2 @@
 #/usr/bin/pwsh
-dotnet publish ./IsbnConverter/IsbnConverter.csproj -c Release -r win-x64 -p:PublishTrimmed=true -p:PublishAot=True 
+dotnet publish ./IsbnConverter/IsbnConverter.csproj -c Release -r win-x64 -p:PublishTrimmed=true -p:PublishAot=True -o ./distrib
